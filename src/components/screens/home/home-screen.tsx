eb3c745da9334ed5198a2ef1988fb23413ce728a
Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>
2021-07-27 14:30:31 +0300

    Update src/components/screens/home/home-screen.tsx

--- src/components/screens/home/home-screen.tsx
+++ src/components/screens/home/home-screen.tsx
@@ -247,7 +247,7 @@ class HomeScreen extends React.Component<Props, State> {
    * @param locale locale
    */
   private setLocale = (locale: string) => {
-    this.setState({ locale: locale });
+    this.setState({ locale });
   }
 
   /**
